6328b0ff670c31018695864a54b3241399f2bb0b
Roth Samreth <140585607+Roth-Samreth@users.noreply.github.com>
2024-05-08 10:15:43 +0700

    exercise with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row p-3 d-flex justify-content-center align-content-center">
+    <div class="row-cols-2">
+      <input class="form-control-sm"
+          type="text"
+          placeholder="Please Enter First Name ..."
+          v-model="first_name"
+      />
+      <input class="form-control-sm"
+          type="text"
+          placeholder="Please Enter Last Name ..."
+          v-model="last_name"
+      />
+      <input class="form-control-sm" type="number" placeholder="Please Enter Age..." v-model="age" />
+      <input class="form-control-sm" type="date" v-model="date_of_birth" />
+    </div>
+    <div class="card row-cols-2">
+      <p>Your first name is: {{ first_name }}</p>
+      <p>Your last name is: {{ last_name }}</p>
+      <p>Your age is: {{ age }}</p>
+      <p>Your full name is: {{ getFullName }}</p>
+      <p :class="{color_status}">Your Your Status: {{ getStatus }}</p>
+      <p>Your date of birth is: {{ date_of_birth }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      first_name: "",
+      last_name: "",
+      age: "",
+      date_of_birth: "",
+      color:'',
+    };
+  },
+  computed: {
+    getFullName() {
+      return this.first_name + " " + this.last_name;
+    },
+    getStatus() {
+      if (!this.age) {
+        return "";
+      } else if (this.age >= 18) {
+        return "Adult";
+      } else {
+        return "Child";
+      }
+    },
+    color_status(){
+      if (!this.age) {
+        return "";
+      } else if (this.age >= 18) {
+        return "bg-primary";
+      } else {
+        return "bg-danger";
+      }
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1 {
+  color: red;
+}
+
+div {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
