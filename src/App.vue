4da1dddfcedad16bbddc18285dd5ad10c4302b1a
Roth Samreth <140585607+Roth-Samreth@users.noreply.github.com>
2024-05-08 08:15:00 +0700

    Work on exercise 03

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row d-flex justify-content-center gap-5">
+    <div class="card col-2" v-bind:style="{background:getColor}">
+      <div class="form-group">
+        <label>First name</label>
+        <input type="text" class="form-control" placeholder="First name" v-model:="firstname">
+        <label for="">Last name</label>
+        <input type="text" class="form-control" placeholder="Last name" v-model:="lastname">
+        <label for="">Style color</label>
+        <input type="color" class="form-control" placeholder="First name" v-model:="text_color">
+      </div>
+    </div>
+    <div class="card col-2" >
+      <h2 v-bind:style="{color:getColor}">{{firstname}} {{lastname}}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      firstname: "",
+      lastname: "",
+      text_color: ""
+    }
+  },
+  computed:{
+    getColor(){
+      return this.text_color
+    }
   }
 }
 </script>
