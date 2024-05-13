3af896aa7957047bdfc7effdef9e534fd54d2a3c
Roth Samreth <140585607+Roth-Samreth@users.noreply.github.com>
2024-05-13 10:17:36 +0700

    Initialize Learn Router

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "AboutPage"
+}
+</script>
+
+<template>
+<h3>About</h3>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<template>
+<h1>Home</h1>
+</template>
+
+<style scoped>
+
+</style>
