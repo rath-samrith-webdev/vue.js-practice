e7ada2e284abd08e12caa37a05d9bcaa5392c172
Your Name <you@example.com>
2024-05-25 19:18:41 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.css";
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap'
 createApp(App).use(bootstrap).mount("#app");
